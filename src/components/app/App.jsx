b9640f05f23af1cab3e92a3af480fbb5726bb812
SamuelHGerber <henrylightfoot@gmail.com>
2021-05-19 08:26:45 -0700

    app rendering and basic file setup

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import AvatarList from '../../containers/AvatarList';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <AvatarList />
+    </>
+  );
 }
