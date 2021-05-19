ceee00469be8f70d9f0ff9b73486f80d6672b2cc
SamuelHGerber <henrylightfoot@gmail.com>
2021-05-19 08:34:59 -0700

    test passing for container loading

--- src/containers/AvatarList.jsx
+++ src/containers/AvatarList.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AvatarList() {
-  const magicPlaceholder = '';
-  return magicPlaceholder;
+  //const [charList, setCharList] = useState();
+  const [loading, setLoading] = useState(true);
+
+  if (loading) return <h2>Loading...</h2>;
+
+  //   return(
+  //       <>
+
+  //       </>
+  //   )
 }
--- src/containers/AvatarList.test.jsx
+++ src/containers/AvatarList.test.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import {render, screen, waitFor} from @testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import AvatarList from './AvatarList';
 import userEvent from '@testing-library/user-event';
-import {rest} from 'msw';
-import {setupServer} from 'msw/node';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 const avatarServer = setupServer(
-    rest.get('URL', (req, res, ctx) => {
-        return res(
-            ctx.json([])
-        )
-    })
+  rest.get('URL', (req, res, ctx) => {
+    return res(ctx.json([]));
+  })
 );
 
 describe('Functional Container Component', () => {
-    beforeAll(() => avatarServer.listen());
-    afterAll(() => avatarServer.close());
+  beforeAll(() => avatarServer.listen());
+  afterAll(() => avatarServer.close());
 
-    it('displays a list of Avatar characters via AvatarList', async () => {
-        render(<AvatarList />);
-    })
-})
+  it('displays a list of Avatar characters via AvatarList', async () => {
+    render(<AvatarList />);
+
+    screen.getByText('Loading...');
+  });
+});
